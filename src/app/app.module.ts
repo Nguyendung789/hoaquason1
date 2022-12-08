46f24097316c70f12de7f7722390b860df571a17
Nguyen Van Vu <nguyenvu20042019@gmail.com>
2022-12-08 22:46:12 +0700

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ContactComponent } from './contact/contact.component';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+
+  { path: '', component: HeaderComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: 'contact', component: ContactComponent }
+];
+
 
 @NgModule({
   declarations: [
@@ -12,7 +22,7 @@ import { ContactComponent } from './contact/contact.component';
     ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,  RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
